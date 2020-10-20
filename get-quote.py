b402ac9cbd4513eff83c45f1aac56ef7fa82e4fa
Paolo Cavanna <paolo.cavanna@drivek.com>
2020-10-20 14:03:06 +0200

    chore(): print first quote0

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,10 @@
 def primary():
-  print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   primary()
